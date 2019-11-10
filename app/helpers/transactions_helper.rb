26f6c67de97f71839db84a95ede64d899739ea29
Avand Amiri <avand@avandamiri.com>
2019-11-09 23:34:33 -0800

    Switch to bar charts with view helper

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,9 @@
 module TransactionsHelper
+  def sum_amounts_by_first_of_month(transactions)
+    transactions.each_with_object(Hash.new(0)) do |transaction, hash|
+      hash["#{transaction.date[0..6]}-01"] += (
+        transaction.amount
+      )
+    end.sort.to_h
+  end
 end
