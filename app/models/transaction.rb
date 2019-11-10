0ad3fd1e39c3e20d3f233e02781eab06e6b565d4
Avand Amiri <avand@avandamiri.com>
2019-11-09 18:28:51 -0800

    More auto-update logic

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,17 +8,25 @@ class Transaction < ApplicationRecord
     "Y#{year_month_day[0]}M#{year_month_day[1]}"
   end
 
-  def auto_update_payments_or_transfers
-    Transaction.where(name: "USAA FUNDS TRANSFER CR").update_all(payment_or_transfer: true)
-    Transaction.where("name LIKE 'TO%USAA FUNDS TRANSFER DB'").update_all(payment_or_transfer: true)
-    Transaction.where(name: "TAX WITHHELD").update_all(payment_or_transfer: true)
-    Transaction.where(name: "INTEREST PAID").update_all(payment_or_transfer: true)
-    Transaction.where("name LIKE 'AUTOPAY %AUTOPAY AUTO-PMT'").update_all(payment_or_transfer: true)
-    Transaction.where("name LIKE 'Online Transfer % transaction%'").update_all(payment_or_transfer: true)
-    Transaction.where(name: "BK OF AMER MC ONLINE PMT").update_all(payment_or_transfer: true)
-    Transaction.where("name like 'AIRBNB, INC DIRECT DEP PPD ID:%'").update_all(payment_or_transfer: true)
-    Transaction.where("name LIKE 'USAA CREDIT CARD PAYMENT % WEB ID: %'").update_all(payment_or_transfer: true)
-    Transaction.where("name LIKE 'AUTOMATIC PAYMENT - THANK%'").update_all(payment_or_transfer: true)
-    Transaction.where(name: 'CHASE CREDIT CRD AUTOPAY').update_all(payment_or_transfer: true)
+  def self.auto_update_payments_or_transfers
+    where(name: "USAA FUNDS TRANSFER CR").update_all(payment_or_transfer: true)
+    where("name LIKE 'TO%USAA FUNDS TRANSFER DB'").update_all(payment_or_transfer: true)
+    where(name: "TAX WITHHELD").update_all(payment_or_transfer: true)
+    where(name: "INTEREST PAID").update_all(payment_or_transfer: true)
+    where("name LIKE 'AUTOPAY %AUTOPAY AUTO-PMT'").update_all(payment_or_transfer: true)
+    where("name LIKE 'Online Transfer % transaction%'").update_all(payment_or_transfer: true)
+    where(name: "BK OF AMER MC ONLINE PMT").update_all(payment_or_transfer: true)
+    where("name like 'AIRBNB, INC DIRECT DEP PPD ID:%'").update_all(payment_or_transfer: true)
+    where("name LIKE 'USAA CREDIT CARD PAYMENT % WEB ID: %'").update_all(payment_or_transfer: true)
+    where("name LIKE 'AUTOMATIC PAYMENT - THANK%'").update_all(payment_or_transfer: true)
+    where(name: 'CHASE CREDIT CRD AUTOPAY').update_all(payment_or_transfer: true)
+  end
+
+  def self.auto_update_allocations
+    where(name: 'GREENTREEPROP ONLINE PMT').update_all(allocation: Account::ALLOCATION_LIVING)
+    where(name: 'GREENVIEW MANAGE 4158303125').update_all(allocation: Account::ALLOCATION_LIVING)
+    where(name: 'SIERRA TELEPHONE COMPA').update_all(allocation: Account::ALLOCATION_KALEIDOSCOPE)
+    where(name: 'SIERRA TELEPHONE CO INC').update_all(allocation: Account::ALLOCATION_KALEIDOSCOPE)
+    # where(name: 'PG&E WEBRECURRING').update_all(allocation: Account::ALLOCATION_KALEIDOSCOPE)
   end
 end
